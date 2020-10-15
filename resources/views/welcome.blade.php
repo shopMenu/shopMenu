15c958699a417b5bc2a2d573861f1216ce5389fc
Your Name <you@example.com>
2020-10-15 18:29:24 +0630

    deleted

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,20 +16,6 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content text-center">
                 <div class="title m-b-md">
                     <h1 id="title" class="text-white p-4 m-4 bg-info">Shop Menu</h1>
